e6581a6e9a053b67cefa52328856400b1fc9fe90
Dorian Lenzner <iwantmyfingerback@gmail.com>
2018-09-15 20:33:15 +0200

    read all objects and return representation

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ import weakref
 
 class Bewerber(object):
 	"""docstring for Bewerber"""
-	_instances = set()
+	_instances = []
 
 	def __init__(self, wahlkreisschluessel, wahlkreisname, wahlvorschlagstraeger, listenplatz, bewerber_im_stimmkreis, titel, firstname, lastname, year_of_birth):
 		super(Bewerber, self).__init__()
@@ -17,25 +17,19 @@ class Bewerber(object):
 		self.firstname = firstname
 		self.lastname = lastname
 		self.year_of_birth = year_of_birth
-		self._instances.add(weakref.ref(self))
+		self._instances.append(self)
 
-	@classmethod
-	def getinstances(cls):
-		dead = set()
-		for ref in cls._instances:
-			obj = ref()
-			if obj is not None:
-				yield obj
-			else:
-				dead.add(ref)
-			cls._instances -= dead
+	def __str__(self):
+		return('' + str(self.firstname) + str(self.lastname) + ' aus der Partei ' + str(self.wahlvorschlagstraeger))
 
 
 with open('ltw18_bewerberdaten.csv') as csvfile:
 	reader = csv.reader(csvfile)
+	next(reader)
 	for row in reader:
-		print(', '.join(row))
+		#print(', '.join(row))
 		Bewerber(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
 
 
-print(Bewerber.getinstances())
+for obj in Bewerber._instances:
+	print(obj)
